06edb103f9a83e62b07f6bb1e2c05922764b7759
tylerhatwigcgi <tylerhatwigcgi@d77a4a04-be32-4cba-adbb-bd6a7233b15d>
2016-06-17 16:51:37 +0000

    Create XML Bindings objects

--- trunk/src/net/scientifichooliganism/xmlplugin/bindings/XMLConfig.java
+++ trunk/src/net/scientifichooliganism/xmlplugin/bindings/XMLConfig.java
@@ -1,12 +1,13 @@
 package net.scientifichooliganism.xmlplugin.bindings;
 
+import net.scientifichooliganism.javaplug.interfaces.Configuration;
+import net.scientifichooliganism.javaplug.vo.BaseConfiguration;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import net.scientifichooliganism.javaplug.vo.Configuration;
-
 @XmlRootElement(name="config")
-public class XMLConfig extends Configuration {
+public class XMLConfig extends BaseConfiguration implements Configuration {
 	public XMLConfig () {
 		super();
 	}
