3d15dfe8747a6948911092f16260e352aca01c8b
tylerhatwigcgi <tylerhatwigcgi@d77a4a04-be32-4cba-adbb-bd6a7233b15d>
2016-08-22 16:07:08 +0000

    update build script to use JUnit, convert XMLPluginTest to JUnit

--- trunk/tst/net/scientifichooliganism/xmlplugin/XMLPluginTest.java
+++ trunk/tst/net/scientifichooliganism/xmlplugin/XMLPluginTest.java
@@ -1,34 +1,52 @@
 package net.scientifichooliganism.xmlplugin;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
-import net.scientifichooliganism.javaplug.annotations.Param;
-import net.scientifichooliganism.javaplug.interfaces.*;
+import net.scientifichooliganism.javaplug.interfaces.Action;
+import net.scientifichooliganism.javaplug.interfaces.Task;
 import net.scientifichooliganism.javaplug.vo.BaseAction;
 import net.scientifichooliganism.javaplug.vo.BaseTask;
-import net.scientifichooliganism.xmlplugin.bindings.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.*;
 
 public class XMLPluginTest {
+    private SimpleDateFormat sdf;
+	private XMLPlugin plugin;
 
 	private XMLPluginTest(){
-
+		sdf = null;
+		plugin = null;
 	}
 
-	public static void main(String args[]){
+	@Before
+	public void init(){
+		sdf = new SimpleDateFormat("yyyy-MM-dd");
+		plugin = XMLPlugin.getInstance();
+	}
+
+	@Test
+	public void constructorTest(){
+		assertNotNull(sdf);
+		assertNotNull(plugin);
+	}
+
+	@Test
+	public void test01(){
+		Action action = new BaseAction();
+		action.setName("My Action Name");
+		action.setMethod("New method");
+		action.setURL("google.com");
+		action.setKlass("test class");
+		action.setDescription("test description");
+		action.setModule("Some module");
+		System.out.println(plugin.stringFromObject(action));
+	}
+
+	@Test
+	public void test02(){
 		try {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			XMLPlugin plugin = XMLPlugin.getInstance();
-
-			Action action = new BaseAction();
-			action.setName("My Action Name");
-			action.setMethod("New method");
-			action.setURL("google.com");
-			action.setKlass("test class");
-			action.setDescription("test description");
-			action.setModule("Some module");
-			System.out.println(plugin.stringFromObject(action));
-
 			Task task = new BaseTask();
 			task.setID("1");
 			task.setLabel("test_label");
@@ -36,8 +54,7 @@ public class XMLPluginTest {
 			task.setDescription("a test task");
 			task.setStartDate(sdf.parse("2016-01-01"));
 			System.out.println(plugin.stringFromObject(task));
-		}
-		catch (Exception exc) {
+		} catch (Exception exc){
 			exc.printStackTrace();
 		}
 	}
